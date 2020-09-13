bd08939a239863c9660a69fb54708894bd583c45
Zyde22 <valentin.crx2@gmail.com>
2020-09-13 16:00:48 +0200

    removed-success-flag

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,26 +2,20 @@ const router = require('express').Router();
 const tweetModel = require('../models/tweet');
 
 router.get('/', (request, response) => {
-  response.status(200).json({ success: true, message: 'Api up' });
+  response.status(200).json({ message: 'Api up' });
 });
 
 router.post('/tweet/create', (request, response) => {
   const { username, tweet } = request.body;
   new tweetModel({ username, tweet })
     .save()
-    .then(() =>
-      response.status(201).json({ success: true, message: 'Tweet created !' })
-    )
-    .catch((err) =>
-      response.status(400).json({ success: false, message: err.message })
-    );
+    .then(() => response.status(201).json({ message: 'Tweet created !' }))
+    .catch((err) => response.status(400).json({ message: err.message }));
 });
 
 router.get('/tweet/all', (request, response) => {
   tweetModel.find({}, (err, tweets) =>
-    err
-      ? response.status(500)
-      : response.status(200).json({ success: true, tweets })
+    err ? response.status(500) : response.status(200).json({ tweets })
   );
 });
 
